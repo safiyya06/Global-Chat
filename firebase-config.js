7aeecb79cd27d8c82828c697491638160d86949b
safiyya06 <UMMI_SAFIYYA_BINTE_SYED_FEROZ@MAI.SG>
2019-08-17 13:04:00 +0800

    to make things neater

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDi-WPTVxMggmeWRWYOPPVVwTP3OBnleF0",
+  authDomain: "global-chat-live-969a9.firebaseapp.com",
+  databaseURL: "https://global-chat-live-969a9.firebaseio.com",
+  projectId: "global-chat-live-969a9",
+  storageBucket: "",
+  messagingSenderId: "242959021173",
+  appId: "1:242959021173:web:b836ca4c5e1bdcd7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
